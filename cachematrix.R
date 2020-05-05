009a39cebafa48a25d515554e58930e0b1142bfd
Andreas <andrewstavrop@gmail.com>
2020-05-05 20:06:35 +0300

    using solve to calculate inverse matrix add comments

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,6 @@
 ## functions do
 
 #use inv from library matlib to calculate the inverse 
-library(matlib)
 
 ## The function makeCacheMatrix gets a matrix as argument. 
 ##Stores the matrix and its inverse as variables 
@@ -17,7 +16,7 @@ set <- function(y) {
   I <<- NULL
 }
 get <- function() x
-setInverse <- function(inv) I <<-inv
+setInverse <- function(Inv) I <<-Inv
 getInverse <- function() I
 list(set = set, get = get,
      setInverse = setInverse,
@@ -25,9 +24,13 @@ list(set = set, get = get,
 }
 
 
-## Write a short comment describing this function
+## If the reverse of matrix created with makeCacheMatrix
+##is already calculated,gets cached data
+##otherwise calculates the inverse matrix
+##and caches it by setting it by using setInverse
+##which is a method of x object which is in global environment
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function() {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getInverse()
   if(!is.null(m)) {
@@ -35,7 +38,7 @@ cacheSolve <- function(x, ...) {
     return(m)
   }
   data <- x$get()
-  m <- inv(data, ...)
+  m <- solve(data)
   x$setInverse(m)
   m
 }
